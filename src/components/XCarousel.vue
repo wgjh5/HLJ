753a584c5d22f0674b12be449980124dc402e902
1806 <462937186@qq.com>
2018-11-03 20:05:48 +0800

    top

---
+++ src/components/XCarousel.vue
@@ -0,0 +1,222 @@
+<template>
+	<section class="Carousel magic-widget" data-track-spm="m-clientapp-index.Carousel_jiao_dian_tu.$.7884db">
+		<div class="carousel-component">
+			<div class="slick-initialized slick-slider">
+				<div class="slick-list">
+					<div class="slick-track" style="opacity: 1; transform: translate3d(-2520px, 0px, 0px); width: 5580px;"><a href="https://p.helijia.com/l/2098?city=440100"
+						 data-track-spm="m-clientapp-index.Carousel_jiao_dian_tu:ImageLink_shuangshiyizaoxing.$.7884db" data-index="-1"
+						 class="slick-slide slick-cloned" style="width: 360px;">
+							<div class="ui-image-component status-show animate animate-end"><img class="image" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_9289604824321e00c0f31642f897b98a.jpg!/0/w-1080/format-webp"><img
+								 class="draft" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_9289604824321e00c0f31642f897b98a.jpg!/0/w-150/quality-10/format-webp"></div>
+						</a><a href="https://p.helijia.com/l/1328?city=440100" data-track-spm="m-clientapp-index.Carousel_jiao_dian_tu:ImageLink_shuangshiyimiaosha.$.7884db"
+						 data-index="0" class="slick-slide" tabindex="-1" style="outline: none; width: 360px;">
+							<div class="ui-image-component status-show animate animate-end"><img class="image" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_0ea47017accb67c9b42d8426a33fb818.jpg!/0/w-1080/format-webp"><img
+								 class="draft" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_0ea47017accb67c9b42d8426a33fb818.jpg!/0/w-150/quality-10/format-webp"></div>
+						</a><a href="https://p.helijia.com/l/2101?city=440100" data-track-spm="m-clientapp-index.Carousel_jiao_dian_tu:ImageLink_shuangshiyipintuan.$.7884db"
+						 data-index="1" class="slick-slide" tabindex="-1" style="outline: none; width: 360px;">
+							<div class="ui-image-component status-show animate animate-end"><img class="image" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_71686612ce15773c65f545f5e5e0e0f3.jpg!/0/w-1080/format-webp"><img
+								 class="draft" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_71686612ce15773c65f545f5e5e0e0f3.jpg!/0/w-150/quality-10/format-webp"></div>
+						</a><a href="https://p.helijia.com/l/2094?city=440100" data-track-spm="m-clientapp-index.Carousel_jiao_dian_tu:ImageLink_shuangshiyimeijia.$.7884db"
+						 data-index="2" class="slick-slide" tabindex="-1" style="outline: none; width: 360px;">
+							<div class="ui-image-component status-show animate animate-end"><img class="image" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_0a6a1d37693be3ba9c1ae31253d2a415.jpg!/0/w-1080/format-webp"><img
+								 class="draft" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_0a6a1d37693be3ba9c1ae31253d2a415.jpg!/0/w-150/quality-10/format-webp"></div>
+						</a><a href="https://p.helijia.com/l/2075?city=440100" data-track-spm="m-clientapp-index.Carousel_jiao_dian_tu:ImageLink_shuangshiyiyimei.$.7884db"
+						 data-index="3" class="slick-slide" tabindex="-1" style="outline: none; width: 360px;">
+							<div class="ui-image-component status-show animate animate-end"><img class="image" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_f1e779b9e0e365fbe4f5beb174cca316.jpg!/0/w-1080/format-webp"><img
+								 class="draft" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_f1e779b9e0e365fbe4f5beb174cca316.jpg!/0/w-150/quality-10/format-webp"></div>
+						</a><a href="https://p.helijia.com/l/2092?city=440100" data-track-spm="m-clientapp-index.Carousel_jiao_dian_tu:ImageLink_shuangshiyimeirong.$.7884db"
+						 data-index="4" class="slick-slide" tabindex="-1" style="outline: none; width: 360px;">
+							<div class="ui-image-component status-show animate animate-end"><img class="image" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_2b2a496dc81d496b7e60ef6b5771eb93.jpg!/0/w-1080/format-webp"><img
+								 class="draft" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_2b2a496dc81d496b7e60ef6b5771eb93.jpg!/0/w-150/quality-10/format-webp"></div>
+						</a><a href="https://p.helijia.com/l/2095?city=440100" data-track-spm="m-clientapp-index.Carousel_jiao_dian_tu:ImageLink_shuangshiyimeijie.$.7884db"
+						 data-index="5" class="slick-slide" tabindex="-1" style="outline: none; width: 360px;">
+							<div class="ui-image-component status-show animate animate-end"><img class="image" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_d612ed121e61c257f850f2cbdc7f48c2.jpg!/0/w-1080/format-webp"><img
+								 class="draft" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_d612ed121e61c257f850f2cbdc7f48c2.jpg!/0/w-150/quality-10/format-webp"></div>
+						</a><a href="https://p.helijia.com/l/2098?city=440100" data-track-spm="m-clientapp-index.Carousel_jiao_dian_tu:ImageLink_shuangshiyizaoxing.$.7884db"
+						 data-index="6" class="slick-slide slick-active slick-current" tabindex="-1" style="outline: none; width: 360px;">
+							<div class="ui-image-component status-show animate animate-end"><img class="image" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_9289604824321e00c0f31642f897b98a.jpg!/0/w-1080/format-webp"><img
+								 class="draft" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_9289604824321e00c0f31642f897b98a.jpg!/0/w-150/quality-10/format-webp"></div>
+						</a><a href="https://p.helijia.com/l/1328?city=440100" data-track-spm="m-clientapp-index.Carousel_jiao_dian_tu:ImageLink_shuangshiyimiaosha.$.7884db"
+						 data-index="7" class="slick-slide slick-cloned" style="width: 360px;">
+							<div class="ui-image-component status-show animate animate-end"><img class="image" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_0ea47017accb67c9b42d8426a33fb818.jpg!/0/w-1080/format-webp"><img
+								 class="draft" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_0ea47017accb67c9b42d8426a33fb818.jpg!/0/w-150/quality-10/format-webp"></div>
+						</a><a href="https://p.helijia.com/l/2101?city=440100" data-track-spm="m-clientapp-index.Carousel_jiao_dian_tu:ImageLink_shuangshiyipintuan.$.7884db"
+						 data-index="8" class="slick-slide slick-cloned" style="width: 360px;">
+							<div class="ui-image-component status-show animate animate-end"><img class="image" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_71686612ce15773c65f545f5e5e0e0f3.jpg!/0/w-1080/format-webp"><img
+								 class="draft" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_71686612ce15773c65f545f5e5e0e0f3.jpg!/0/w-150/quality-10/format-webp"></div>
+						</a><a href="https://p.helijia.com/l/2094?city=440100" data-track-spm="m-clientapp-index.Carousel_jiao_dian_tu:ImageLink_shuangshiyimeijia.$.7884db"
+						 data-index="9" class="slick-slide slick-cloned" style="width: 360px;">
+							<div class="ui-image-component status-show animate animate-end"><img class="image" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_0a6a1d37693be3ba9c1ae31253d2a415.jpg!/0/w-1080/format-webp"><img
+								 class="draft" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_0a6a1d37693be3ba9c1ae31253d2a415.jpg!/0/w-150/quality-10/format-webp"></div>
+						</a><a href="https://p.helijia.com/l/2075?city=440100" data-track-spm="m-clientapp-index.Carousel_jiao_dian_tu:ImageLink_shuangshiyiyimei.$.7884db"
+						 data-index="10" class="slick-slide slick-cloned" style="width: 360px;">
+							<div class="ui-image-component status-show animate animate-end"><img class="image" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_f1e779b9e0e365fbe4f5beb174cca316.jpg!/0/w-1080/format-webp"><img
+								 class="draft" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_f1e779b9e0e365fbe4f5beb174cca316.jpg!/0/w-150/quality-10/format-webp"></div>
+						</a><a href="https://p.helijia.com/l/2092?city=440100" data-track-spm="m-clientapp-index.Carousel_jiao_dian_tu:ImageLink_shuangshiyimeirong.$.7884db"
+						 data-index="11" class="slick-slide slick-cloned" style="width: 360px;">
+							<div class="ui-image-component status-show animate animate-end"><img class="image" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_2b2a496dc81d496b7e60ef6b5771eb93.jpg!/0/w-1080/format-webp"><img
+								 class="draft" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_2b2a496dc81d496b7e60ef6b5771eb93.jpg!/0/w-150/quality-10/format-webp"></div>
+						</a><a href="https://p.helijia.com/l/2095?city=440100" data-track-spm="m-clientapp-index.Carousel_jiao_dian_tu:ImageLink_shuangshiyimeijie.$.7884db"
+						 data-index="12" class="slick-slide slick-cloned" style="width: 360px;">
+							<div class="ui-image-component status-show animate animate-end"><img class="image" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_d612ed121e61c257f850f2cbdc7f48c2.jpg!/0/w-1080/format-webp"><img
+								 class="draft" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_d612ed121e61c257f850f2cbdc7f48c2.jpg!/0/w-150/quality-10/format-webp"></div>
+						</a><a href="https://p.helijia.com/l/2098?city=440100" data-track-spm="m-clientapp-index.Carousel_jiao_dian_tu:ImageLink_shuangshiyizaoxing.$.7884db"
+						 data-index="13" class="slick-slide slick-cloned" style="width: 360px;">
+							<div class="ui-image-component status-show animate animate-end"><img class="image" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_9289604824321e00c0f31642f897b98a.jpg!/0/w-1080/format-webp"><img
+								 class="draft" src="https://img-ucdn-static.helijia.com/zmw/upload/magic/images/original_9289604824321e00c0f31642f897b98a.jpg!/0/w-150/quality-10/format-webp"></div>
+						</a></div>
+				</div>
+				<ul class="slick-dots" style="display: block;">
+					<li class=""><button>1</button></li>
+					<li class=""><button>2</button></li>
+					<li class=""><button>3</button></li>
+					<li class=""><button>4</button></li>
+					<li class=""><button>5</button></li>
+					<li class=""><button>6</button></li>
+					<li class="slick-active"><button>7</button></li>
+				</ul>
+			</div>
+		</div>
+	</section>
+</template>
+<script>
+	export default {
+		components: {}
+	}
+</script>
+
+<style scoped>
+.carousel-component {
+    max-height: 12rem;
+}
+.slick-slider {
+    position: relative;
+    display: block;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    -webkit-touch-callout: none;
+    -khtml-user-select: none;
+    -ms-touch-action: pan-y;
+    touch-action: pan-y;
+    -webkit-tap-highlight-color: transparent;
+}
+*, *:after, *:before {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+}
+.slick-slider .slick-track, .slick-slider .slick-list {
+    -webkit-transform: translate3d(0, 0, 0);
+    -ms-transform: translate3d(0, 0, 0);
+    transform: translate3d(0, 0, 0);
+}
+.slick-list {
+    position: relative;
+    display: block;
+    overflow: hidden;
+    margin: 0;
+    padding: 0;
+}
+.carousel-component .slick-dots {
+    bottom: 8px;
+}
+.slick-dots {
+    position: absolute;
+    bottom: -25px;
+    display: block;
+    width: 100%;
+    padding: 0;
+    margin: 0;
+    list-style: none;
+    text-align: center;
+}
+ul, li, dl {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+}
+.carousel-component .slick-dots > li {
+    width: auto;
+    height: auto;
+    margin: 0 4px;
+}
+.slick-dots li {
+    position: relative;
+    display: inline-block;
+    width: 20px;
+    height: 20px;
+    margin: 0 5px;
+    padding: 0;
+    cursor: pointer;
+}
+.carousel-component .slick-dots > li button {
+    opacity: .5;
+    background-color: #fff;
+    border-radius: 50%;
+    width: 6px;
+    height: 6px;
+    padding: 0;
+}
+.slick-dots li button {
+    font-size: 0;
+    line-height: 0;
+    display: block;
+    width: 20px;
+    height: 20px;
+    padding: 5px;
+    cursor: pointer;
+    color: transparent;
+    border: 0;
+    outline: none;
+    background: transparent;
+}
+button, select {
+    text-transform: none;
+}
+button, input, optgroup, select, textarea {
+    font-family: inherit;
+    font-size: 100%;
+    line-height: 1.15;
+    margin: 0;
+}
+.slick-track {
+    position: relative;
+    top: 0;
+    left: 0;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+.slick-initialized .slick-slide {
+    display: block;
+}
+.slick-slide {
+    display: none;
+    float: left;
+    height: 100%;
+    min-height: 1px;
+}
+a {
+    text-decoration: none;
+    color: #1a1a1a;
+}
+a {
+    background-color: transparent;
+}
+.slick-track:before, .slick-track:after {
+    display: table;
+    content: '';
+}
+.slick-track:after {
+    clear: both;
+}
+.slick-track:before, .slick-track:after {
+    display: table;
+    content: '';
+}
+</style>
--- src/containers/Home.vue
+++ src/containers/Home.vue
@@ -1,5 +1,8 @@
 <template>
     <div>
+		<XTopbar />
+		<XCarousel />
+		<XGridMenu />
         <XsmallImglink />
         <Xproduct />
         <XbigImglink />
@@ -15,6 +18,9 @@
     </div>
 </template>
 <script>
+import XTopbar from '../components/XTopbar.vue';
+import XCarousel from '../components/XCarousel.vue';
+import XGridMenu from '../components/XGridMenu.vue';
 import XsmallImglink from '../components/XsmallImglink.vue';
 import Xproduct from '../components/Xproduct.vue';
 import XbigImglink from '../components/XbigImglink.vue';
@@ -22,6 +28,9 @@ import Xspace from '../components/Xspace.vue';
 import Xrecommend from '../components/Xrecommend.vue';
 export default {
     components: {
+		XTopbar,
+		XCarousel,
+		XGridMenu,
         XsmallImglink,
         Xproduct,
         XbigImglink,
@@ -31,4 +40,5 @@ export default {
 }
 </script>
 <style scoped>
+	
 </style>
