486e4941ce1481523aa23df8b401ca61a5c7da86
wgj <email@qqcom>
2018-11-03 17:53:54 +0800

    河狸家

--- src/main.js
+++ src/main.js
@@ -3,6 +3,28 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// =====vue路由
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+
+// ====vuex
+import Vuex from 'vuex'
+Vue.use(Vuex)
+import Home from './containers/Home.vue'
+
+const routes = [{
+        path: '/home',
+        name: 'home',
+        component: Home,
+    },
+    { path: '/', redirect: { name: 'home' } }
+]
+const router = new VueRouter({
+    routes // (缩写) 相当于 routes: routes
+
+})
 new Vue({
-  render: h => h(App),
+    router,
+    render: h => h(App),
 }).$mount('#app')
